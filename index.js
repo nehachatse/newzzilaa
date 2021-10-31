53b8e8a3785dfbe1ae6fbac827706b0e685485d7
nehachatse <nehachatse02@gmail.com>
2021-10-31 17:18:20 +0530

    Final commit

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-
+        var curretPage = 1;
+        var limit = 5;
+        var ele;
         var api_key = "c16cb19662a447c39a3840d54b085f3d";
+
+        window.addEventListener('load',function(){
+            var btn = document.getElementById('btn');
+            btn.addEventListener('click', search);
+
+            createPagination();
+            var pagin = document.getElementById('pagination');
+            pagin.addEventListener('click', handlePagination);
+        });
+
         fetchNews();
 
         async function fetchNews(){
@@ -16,7 +28,6 @@
 
         function loading(value){
             var load = document.getElementById('loading');
-            console.log(load, value)
             if(value){
                 load.className = 'visible'
             }
@@ -126,5 +137,144 @@
 
             page.appendChild(container)
             loading(false)
+            
+        }
+
+        function createPagination(){
+            var pagination = document.getElementById("pagination");
+            pagination.textContent = "";
+
+            var prev = document.createElement('button');
+            prev.textContent = "Previous";
+            prev.name = curretPage - 1;
+            if(curretPage <= 1){
+                prev.disabled = true;
+            }
+
+            var current = document.createElement('button');
+            current.name = curretPage;
+            current.textContent = curretPage;
+
+            var next = document.createElement('button');
+            next.name = curretPage + 1;
+            next.textContent = "Next";
+
+            pagination.append(prev, current, next);
+        }
+
+        function handlePagination(e){
+            const pageNum = parseInt(e.target.name);
+            curretPage = pageNum;
+            fetchUserData(curretPage, limit);
+            createPagination();
+        }
+
+        function search(){
+            ele = document.getElementById('input').value;
+            console.log(ele)
+            fetchUserData(curretPage, limit);
+        }
+
+        function fetchUserData(curretPage, limit){
+            console.log("I'm")
+            fetch(`https://newsapi.org/v2/everything?q=${ele}&page=${curretPage}&pageSize=${limit}&apiKey=${api_key}`)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(response){
+                console.log(response);
+                response = response.articles;
+                console.log(response);
+                postNewsOfSearch(response)
+            })
+            .catch(function(err){
+                console.log(err);
+            })
+        }
+
+        function postNewsOfSearch(newsData){
+            console.log("I'm============")
+            var page = document.getElementById('page');
+            page.textContent = "";
+
+            var container = document.createElement('div');
+            container.setAttribute('id', 'container')
+
+            for(let i = 0; i < newsData.length; i++){
+                if(i === 0){
+                    var top = document.createElement('div');
+                    top.style.flexBasis = '100%';
+                    top.style.height = '550px';
+                    top.style.zIndex = -1;
+                    top.style.position = 'relative'
+
+                    var img = document.createElement('img');
+                    img.src = newsData[i].urlToImage;
+                    img.style.width = '100%';
+                    img.style.height = '550px';
+
+                    var hoverDiv = document.createElement('div');
+                hoverDiv.setAttribute('id', 'hoverDiv');
+                
+
+                var headline = document.createElement('h1');
+                headline.textContent = newsData[i].title;
+                headline.style.margin = '100px 0px 0px 50px';
+                headline.style.width = '50%'
+                headline.style.fontSize = '40px'
+
+                var content = document.createElement('p');
+                content.textContent = newsData[i].content;
+                content.style.margin = '20px 50px';
+                content.style.width = '50%'
+                
+
+                var detail1 = document.createElement('h3');
+                detail1.textContent = `Author -  ${newsData[i].author}`;
+                detail1.style.margin = '20px 50px';
+                detail1.style.width = '50%'
+
+                var detail2 = document.createElement('h4');
+                detail2.textContent = `Published At -  ${newsData[i].publishedAt}`;
+                detail2.style.margin = '20px 50px';
+                detail2.style.width = '50%'
+
+                hoverDiv.append(headline, content, detail1, detail2);
+                top.append(img, hoverDiv);
+                container.appendChild(top);
+                }
+
+                else{
+                    var top = document.createElement('div');
+                    top.setAttribute('id', 'flexDivs');
+
+                    var img = document.createElement('img');
+                    img.src = newsData[i].urlToImage;
+                    img.setAttribute('class', 'img');
+
+                    var div = document.createElement('div');
+                    div.style.margin = '10px' 
+
+                    var headline = document.createElement('h3');
+                    headline.textContent = newsData[i].title;
+
+                    var content = document.createElement('p');
+                    content.textContent = newsData[i].content;  
+                    content.style.margin = '10px 0'                  
+
+                    var detail1 = document.createElement('h4');
+                    detail1.textContent = `Author -  ${newsData[i].author}`;
+
+                    var detail2 = document.createElement('h4');
+                    detail2.textContent = `Published At -  ${newsData[i].publishedAt}`;
+
+                    div.append(headline, content, detail1, detail2);
+                    top.append(img, div)
+                    container.appendChild(top);
+
+                }
+            }
+
+            page.appendChild(container)
             
         }
